aaa96edfedaa434d17b9ca1de652b0bec50dfb01
Juan Almeida <juanalmeidasantos@gmail.com>
2023-06-06 22:52:26 -0300

    js resolvido por parte

--- js/wpp.js
+++ js/wpp.js
@@ -1,15 +1,15 @@
-
-function abrirWhatsapp() {
-    var nome = document.getElementById("nome").value;
-    var telefone = document.getElementById("telefone").value;
-    var email = document.getElementById("email").value;
-    var msg = document.getElementById("msg").value;
-    var url = "https://wa.me/5511994410421?text=" // Seu numero
-      + "*Orçamento*" + "%0a" // Mensagem personalizada
-      + "%0a" // Quebra de linha
-      + "*Nome*: " + nome + "%0a" // Dados do formulário
-      + "*Telefone*: " + telefone + "%0a"
-      + "*E-mail*: " + email + "%0a"
-      + "*Mensagem*: " + mensagem;
-    window.open(url, '_blank').focus();
-  }
+function wpp() {
+  var nome = document.getElementById("nome").value;
+  var telefone = document.getElementById("telefone").value;
+  var email = document.getElementById("email").value;
+  var msg = document.getElementById("msg").value;
+  var url = "https://wa.me/5511994410421?text=" // Seu numero
+    + "*Orçamento*" + "%0a" // Mensagem personalizada
+    + "%0a" // Quebra de linha
+    + "*Nome*: " + nome + "%0a" // Dados do formulário
+    + "*Telefone*: " + telefone + "%0a"
+    + "*E-mail*: " + email + "%0a"
+    + "*Mensagem*: " + msg;
+  window.open(url, '_blank').focus();
+  event.preventDefault();
+}
