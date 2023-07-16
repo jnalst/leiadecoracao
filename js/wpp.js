e1cd06eb49f939b1832f3782d027506df8809217
Juan Almeida <117415730+jnalst@users.noreply.github.com>
2023-07-16 17:35:58 -0300

    Add files via upload

--- js/wpp.js
+++ js/wpp.js
@@ -24,7 +24,8 @@ function wpp() {
   var msg = document.getElementById("msg").value;
   var url = "https://wa.me/5511994410421?text=" // Seu numero
     + "*Orçamento*" + "%0a" // Mensagem personalizada
-    + "*Sobre*" + validarVariaveis() + "%0a" 
+    + "%0a" // Quebra de linha
+    + "*Sobre*: " + validarVariaveis() + "%0a" 
     + "%0a" // Quebra de linha
     + "*Nome*: " + nome + "%0a" // Dados do formulário
     + "*Telefone*: " + telefone + "%0a"
